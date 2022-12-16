44349a7aa1cbc73ff2cb940e7f8780b2bc8fa44b
Jean Paul <jeanpaulbenitescoico@gmail.com>
2022-12-16 12:29:17 -0500

    registroSemiTerminado

--- codigo.js
+++ codigo.js
@@ -1,5 +1,5 @@
 function doGet() {
-    var template = HtmlService.createTemplateFromFile('index');
+    var template = HtmlService.createTemplateFromFile('registro');
     var output = template.evaluate();
     var htmlOutput = HtmlService.createHtmlOutput(output);   
     htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');   
@@ -9,3 +9,40 @@ function doGet() {
 function include(filename){
     return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
+
+function registrar(nombre, apellido, correo, contrasenia){
+    var id = "1UHpnj6UErcsd0-6VZ3J7g6wcWCv1ACt-KLrxnowOmAc";
+    var SS = SpreadsheetApp.openById(id);
+    var ws = SS.getSheetByName("usuarios");
+    var obj = {};
+  
+    if(repetido(correo)=='false'){
+        obj={status: "404", error: 'unknown' }
+    }
+    else{
+        obj={status: "200"}
+        ws.appendRow([correo,`'${contrasenia}`,`'${nombre}`,`'${apellido}`]);
+ 
+    }
+  
+    return obj;
+}
+
+function repetido(x){
+    var id = "1UHpnj6UErcsd0-6VZ3J7g6wcWCv1ACt-KLrxnowOmAc";
+    var ss = SpreadsheetApp.openById(id);
+    var colcta = ss.getSheetByName("usuarios");
+    var ctaDates = colcta.getDataRange().getValues();
+    var a='';
+    for (var fila = 1; fila < ctaDates.length; fila++) {
+      var correo= ctaDates[fila][0];
+      
+      if(x.toString()=== correo.toString()){
+        a='false';
+        break;
+      }
+  
+    }
+    return a;
+  
+  }
